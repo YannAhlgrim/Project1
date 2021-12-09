d51450943cb3677a2feeb4ab26c4bda301cc4dd4
frenchie71 <marc.ahlgrim@web.de>
2021-12-09 12:38:23 +0100

    added error handler

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,14 @@
 public class HelloWorld {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello World!");
-		System.out.println("change");
-		System.out.println("change2");
+		try {
+			System.out.println("Hello World!");
+			System.out.println("change");
+			System.out.println("change2");
+		}
+		catch (e) {
+			System.out.println("There was an error");
+		}			
 	}
 
 }
